a7442cf1749d62c36fce33bc21088f170799f619
Lighthigh57 <78249745+Lighthigh57@users.noreply.github.com>
2022-04-05 15:38:49 +0900

    ShowGameView

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Transform board;
     [SerializeField] private Material[] materials;
 
+    private InterstitialAdExample interstitialAdExample;
+
     /// <summary>
     /// 盤上の情報を格納(たて、奥行き、よこ)
     /// </summary>
@@ -16,16 +19,19 @@ public class GameManager : MonoBehaviour
     /// 棒に何個入っているか(奥行き、よこ)
     /// </summary>
     private readonly int[,] onStick = new int[4, 4];
-
-    private InterstitialAdExample interstitialAd;
-    private int stone = 1;
-    const int distance = 10;
-    const int duration = 3;
+    /// <summary>
+    /// 白が1 黒が-1
+    /// </summary>
+    private int turn = 1;
+    private bool nowPlaying = true;
+    private const int distance = 10;
+    private const int duration = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        interstitialAd = GetComponent<InterstitialAdExample>();
+        GetComponent<AdsInitializer>().InitializeAds();
+        interstitialAdExample = GetComponent<InterstitialAdExample>();
         for (int i = 0; i < 4; i++)
         {
             for (int j = 0; j < 4; j++)
@@ -40,51 +46,114 @@ public class GameManager : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && nowPlaying)
         {
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Debug.DrawRay(ray.origin, ray.direction * distance, Color.red, duration, false);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                Vector3 vector = hit.transform.position;
-                int x = (int)(vector.x + 1.5f);
-                int z = (int)Mathf.Abs(vector.z - 1.5f);
-                if (onStick[z, x] < 4)
+                if (hit.transform.CompareTag("SelectPoint"))
                 {
-                    vector.y = 8;
+                    Vector3 vector = hit.transform.position;
+                    int x = (int)(vector.x + 1.5f);
+                    int z = (int)Mathf.Abs(vector.z - 1.5f);
+                    if (onStick[z, x] < 4)
+                    {
+                        vector.y = 8;
 
-                    GameObject game = Instantiate(sphere, vector, Quaternion.identity);
-                    game.GetComponent<Renderer>().material = materials[stone - 1];
+                        GameObject game = Instantiate(sphere, vector, Quaternion.identity);
+                        game.GetComponent<Renderer>().material = materials[Mathf.Abs(turn - 1) / 2];
 
-                    field[onStick[z, x], z, x] = stone;
-                    if (stone == 1)
-                        stone++;
-                    else
-                        stone--;
-                    Jadge(onStick[z, x]++, z, x, stone);
+                        field[onStick[z, x], z, x] = turn;
+                        
+                        if (Jadge(onStick[z, x]++, z, x))
+                        {
+                            Debug.Log("GameSet!");
+                            StartCoroutine(GameSet());
+                            nowPlaying = false;
+                        }
+                        turn *= -1;
+                    }
                 }
-
-                //interstitialAd.LoadAd();
-                //interstitialAd.ShowAd();
 
             }
         }
     }
-    private void Jadge(int y, int z, int x, int stone)
+
+    private IEnumerator GameSet()
     {
-        bool[] state = new bool[3];
+        Time.timeScale = 0.5f;
+        yield return new WaitForSecondsRealtime(1.5f);
+        Time.timeScale = 1;
+        yield return null;
+    }
+
+    public void GameRestart()
+    {
+        interstitialAdExample.LoadAd();
+        interstitialAdExample.ShowAd();
+        // 現在のSceneを取得
+        Scene loadScene = SceneManager.GetActiveScene();
+        // 現在のシーンを再読み込みする
+        SceneManager.LoadScene(loadScene.name);
+    }
+
+    public void GameExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+        Application.Quit();//ゲームプレイ終了
+#endif
+    }
+
+    private bool Jadge(int y, int z, int x)
+    {
+        int[] sum = new int[3];
+
         for (int i = 0; i < 4; i++)
         {
-            if (!(field[i, z, x] == stone + 1))
-                state[0] = true;
-            if (!(field[y, i, x] == stone + 1))
-                state[1] = true;
-            if (!(field[y, z, i] == stone + 1))
-                state[2] = true;
-            if(state[0] == state[1] == state[2] == true)
-                return;
+            sum[0] += field[i, z, x];
+            sum[1] += field[y, i, x];
+            sum[2] += field[y, z, i];
         }
-        SceneManager.LoadScene("result");
+        foreach (int i in sum)
+        {
+            if (Mathf.Abs(i) == 4)
+                return true;
+        }
+
+        int sumX = 0;
+
+        if ((z * 4 + x) % 3 == 0)
+        {
+            for (int i = 0; i < 4; i++)
+                sumX += field[y, i, 3 - i];
+        }
+        else if (((z * 4) + x) % 5 == 0)
+        {
+            for (int i = 0; i < 4; i++)
+                sumX += field[y, i, i];
+        }
+        if (Mathf.Abs(sumX) == 4)
+            return true;
+
+        int[] sumDX = new int[4];
+        for (int i = 0; i < 4; i++)
+        {
+            sumDX[0] = field[i, i, i];
+            sumDX[1] = field[3 - i, i, i];
+            sumDX[2] = field[i, i, 3 - i];
+            sumDX[3] = field[3 - i, i, 3 - i];
+        }
+        foreach (int i in sumDX)
+        {
+            if (Mathf.Abs(i) == 4)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
